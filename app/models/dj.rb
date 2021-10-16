ebaae4e5d7fde3479f6821cef7e4348fbb53808c
Casey <cantrellcasey@gmail.com>
2021-10-16 14:12:22 -0500

    finishes routes, controller actions, and requests file

--- app/models/dj.rb
+++ app/models/dj.rb
@@ -2,5 +2,8 @@ class Dj < ApplicationRecord
 
   has_secure_password
   validates :email, presence: true, uniqueness: true
+  validates :name, presence:true, uniqueness: true
 
+  has_many :requests
+  
 end
