1d67785540072378da3308d75e4157531762569f
Casey <cantrellcasey@gmail.com>
2021-10-16 13:51:53 -0500

    seeds requests

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 
 Request.create!(dj_id: "2", song: "Party Up by DMX", comments: "Thanks for playing this, you're the best DJ ever", status: "approved")
 
-Request.create!(dj_id: "3", song: "casey@casey.com", comments: "Play this song or my group and I are going to leave", status: "declined")
+Request.create!(dj_id: "3", song: "Friday by Rebecca Black", comments: "Play this song or my group and I are going to leave", status: "declined")
 
-Request.create!(dj_id: "4", song: "emailcaseyrells@gmail.com", comments: "quincyboi", status: "")
+Request.create!(dj_id: "4", song: "Drop It Like It's Hot by Snoop Dogg", comments: "It's my birthday and I would love to hear this song! Thank you!", status: "approved")
