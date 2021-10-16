2f7a99630e3ca24447ca729e447cfd0d4ef28a79
Casey <cantrellcasey@gmail.com>
2021-10-16 13:36:05 -0500

    adds update and destroy actions

--- app/controllers/djs_controller.rb
+++ app/controllers/djs_controller.rb
@@ -19,4 +19,30 @@ class DjsController < ApplicationController
     render json: dj
   end
 
+  def update
+    dj = Dj.find(params[:id])
+    dj.name = params[:name] || dj.name
+    dj.info = params[:info] || dj.info
+    dj.website = params[:website] || dj.website
+    dj.image_url = params[:image_url] || dj.image_url
+    dj.instagram = params[:instagram] || dj.instagram
+    dj.twitter = params[:twitter] || dj.twitter
+    dj.facebook = params[:facebook] || dj.facebook
+    dj.venmo = params[:venmo] || dj.venmo
+    dj.cashapp = params[:cashapp] || dj.cashapp
+    dj.paypal = params[:paypal] || dj.paypal
+    dj.email = params[:email] || dj.email
+      if dj.save
+      render json: dj
+      else
+      render json: dj.errors.full_messages, status: :unprocessable_entity
+      end
+  end
+
+  def destroy
+    dj = Dj.find_by(id: params[:id])
+    dj.destroy
+    render json: { message: "Successfully DESTROYED!" }
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Dj.create!(name: "test", email: "test@test.com", password: "test", password_confirmation: "test")
-Dj.create!(name: "casey", email: "casey@casey.com", password: "casey", password_confirmation: "casey")
-Dj.create!(name: "Casey Rells", email: "emailcaseyrells@gmail.com", password: "quincyboi", password_confirmation: "quincyboi")
+
+# Dj.create!(name: "test", email: "test@test.com", password: "test", password_confirmation: "test")
+# Dj.create!(name: "casey", email: "casey@casey.com", password: "casey", password_confirmation: "casey")
+# Dj.create!(name: "Casey Rells", email: "emailcaseyrells@gmail.com", password: "quincyboi", password_confirmation: "quincyboi")
+
