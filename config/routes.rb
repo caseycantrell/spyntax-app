68b2418c052cafae8bfd0321dcd2bb751c0d9244
Casey <cantrellcasey@gmail.com>
2021-11-13 13:23:36 -0600

    fixes quotes on route file

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   
-  post "/djs" => "djs#create"
-  get "/djs/:id" => "djs#show"
-  patch "/djs/me" => "djs#update"
-  delete "/djs/me" => "djs#destroy"
+  post '/djs' => 'djs#create'
+  get '/djs/:id' => 'djs#show'
+  patch '/djs/me' => 'djs#update'
+  delete '/djs/me' => 'djs#destroy'
 
-  get "/requests" => "requests#index"
-  post "/requests" => "requests#create"
-  patch "/requests/:id" => "requests#update"
-  delete "/requests/all" => "requests#destroy"
+  get '/requests' => 'requests#index'
+  post '/requests' => 'requests#create'
+  patch '/requests/:id' => 'requests#update'
+  delete '/requests/all' => 'requests#destroy'
 
-  post "/sessions" => "sessions#create"
+  post '/sessions' => 'sessions#create'
 
   mount ActionCable.server => '/cable'
   
