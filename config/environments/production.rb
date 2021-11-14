c185ab1af950578168b1bdb0a8cce3ef1926aad9
Casey <cantrellcasey@gmail.com>
2021-11-13 18:36:43 -0600

    fixes action cable urls

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://spyntax.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://spyntax.herokuapp.com', /http:\/\/spyntax.herokuapp.com.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://spyntax.netlify.app', /http:\/\/spyntax.netlify.app.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
